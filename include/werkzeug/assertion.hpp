d24fef3aa36b71df528c2445a5648394f52f76e3
IyeOnline <Iye@fantasymail.de>
2024-12-30 17:12:52 +0100

    Improve assertion library

--- include/werkzeug/assertion.hpp
+++ include/werkzeug/assertion.hpp
@@ -4,7 +4,8 @@
 
 #include <stdexcept>
 #include <iostream>
-#include <cassert>
+#include <source_location>
+#include <format>
 
 namespace werkzeug
 {
@@ -15,40 +16,33 @@ namespace werkzeug
 
 	namespace detail
 	{
-		inline void print_assert_location( std::ostream& os, const char* const src_loc, const char* const function) noexcept
-		{
-			os << "Location:\n\t" << src_loc << "\nFunction:\n\t" << function << '\n';
-		}
-
-		inline void print_assert_condition( std::ostream& os, const char* const condition ) noexcept
-		{
-			os << "Condition:\n\t" << condition << '\n';
-		}
-
 		template<typename ... Ts>
-		inline void print_assert_message( std::ostream& os, Ts&& ... explanation ) noexcept
+		void print_assert_message( std::ostream& os, const char* condition, std::source_location loc, std::format_string<Ts...> fmt, Ts&& ... args )
 		{
-			os << "Explanation:\n\t";
-			( (os << explanation << '\n'), ... );
+			os << "Location:\n\t" << loc.file_name() << ":" << loc.function_name() << ":" << loc.line() << '\n';
+			os << "Condition:\n\t" << condition << '\n';
+			os << "Details:\n\t" << std::format( fmt, std::forward<Ts>(args) ... ) << '\n';
 		}
 	}
 }
 
+#define WERKZEUG_ASSERT_ALWAYS( condition, ... ) \
+if ( not static_cast<bool>( condition ) ) \
+{ \
+	std::cerr << "ASSERTION FAILED:\n"; \
+	::werkzeug::detail::print_assert_message( std::cerr, #condition, std::source_location::current() __VA_OPT__(,) __VA_ARGS__ ); \
+	std::terminate(); \
+} \
+
+#define WERKZEUG_WARN_ALWAYS( condition, ... ) \
+if ( not static_cast<bool>( condition ) ) \
+{ \
+	std::cerr << "Warning:\n"; \
+	::werkzeug::detail::print_assert_message( std::cerr, #condition, std::source_location::current() __VA_OPT__(,) __VA_ARGS__ ); \
+} \
+
 
 #ifndef NDEBUG
-	#if defined __GNUC__
-		#define WERKZEUG_GUARD_FUNCTION_NAME __PRETTY_FUNCTION__
-	#elif defined _MSC_VER
-		#define WERKZEUG_GUARD_FUNCTION_NAME __FUNCSIG__	
-	#else
-		#define WERKZEUG_GUARD_FUNCTION_NAME __func__
-	#endif
-
-	#define WERKZEUG_GUARD_STRINGIFY_IMPL(x) #x
-    #define WERKZEUG_GUARD_STRINGIFY( x ) WERKZEUG_GUARD_STRINGIFY_IMPL(x)
-	#define WERKZEUG_GUARD_SRC_LOC __FILE__ ":" WERKZEUG_GUARD_STRINGIFY(__LINE__) ":"
-
-
 	/**
 	 * @brief custom assert macro that takes in a (string like) error message.
 	 * Please create the error message *inside* of the macro as to not impede release performance
@@ -56,32 +50,8 @@ namespace werkzeug
 	 * @param condition the condition to test for truth
 	 * @param message A string like message to be printed if the assertion fails. Please create the error message *inside* the macro to ensure it doesnt impede release performance
 	 */
-	#define WERKZEUG_ASSERT( condition, message ) \
-	if ( not (condition)) \
-	{ \
-		std::cerr << "ASSERTION FAILED.\n"; \
-		werkzeug::detail::print_assert_location( std::cerr, WERKZEUG_GUARD_SRC_LOC, WERKZEUG_GUARD_FUNCTION_NAME ); \
-		werkzeug::detail::print_assert_condition( std::cerr, #condition ); \
-		werkzeug::detail::print_assert_message( std::cerr, message ); \
-		std::terminate(); \
-	} \
-	assert( condition ) //also use a language level assert to potentially communicate to the optmizer. Currently too lazy to create a custom portable ASSUME macro
-
-	#define WERKZEUG_WARN( condition, message )\
-	if ( not (condition) ) \
-	{ \
-		std::clog << "Warning:\n"; \
-		detail::print_assert_location( std::clog, WERKZEUG_GUARD_SRC_LOC, WERKZEUG_GUARD_FUNCTION_NAME ); \
-		detail::print_assert_condition( std::clog, #condition ); \
-		detail::print_assert_message( std::clog, message ); \
-	} (void)0
-
-	#define WERKZEUG_WARN_ALWAYS( message ) \
-	{ \
-		std::clog << "Warning:\n"; \
-		detail::print_assert_location( std::clog, WERKZEUG_GUARD_SRC_LOC, WERKZEUG_GUARD_FUNCTION_NAME ); \
-		detail::print_assert_message( std::clog, message ); \
-	} (void)0
+	#define WERKZEUG_ASSERT( condition, ... ) WERKZEUG_ASSERT_ALWAYS( condition __VA_OPT__(,) __VA_ARGS__ )
+	#define WERKZEUG_WARN( condition, ... ) WERKZEUG_WARN_ALWAYS( condition __VA_OPT__(,) __VA_ARGS__ )
 #else
 	/**
 	 * @brief custom assert macro that takes in a (string like) error message.
@@ -90,10 +60,8 @@ namespace werkzeug
 	 * @param condition the condition to test for truth
 	 * @param message A string like message to be printed if the assertion fails. Please create the error message *inside* the macro to ensure it doesnt impede release performance
 	 */
-	#define WERKZEUG_ASSERT( condition, message ) (void)0
-	#define WERKZEUG_WARN( condition, message ) (void)0
-	#define WERKZEUG_WARN_ALWAYS( message ) (void)0
-
+	#define WERKZEUG_ASSERT( condition, ... ) (void)0
+	#define WERKZEUG_WARN( condition, ... ) (void)0
 #endif
 
 
